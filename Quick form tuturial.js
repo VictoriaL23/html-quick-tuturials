ba86702dc7afc1e53ad5592c0c938f8100b9310a
VictoriaL23 <83440406+VictoriaL23@users.noreply.github.com>
2023-01-28 15:54:29 -0500

    Update Quick form tuturial.js

--- Quick form tuturial.js
+++ Quick form tuturial.js
@@ -1,13 +1,25 @@
 <html lang="en">
-    <head>
-        <meta charset="utf-8"/>
-        <title>Forms </title>
-        <script type="text/javascript">
+<head>
+    <meta charset="utf-8"/>
+    <title>Forms </title>
+    <script type="text/javascript">
 
-        </script>
-    </head>    
-    <body>
-    <p id = "Welcome to the form"> </p>    
-        </body>    
+            function write_name() {
+                var welcome_tori = document.getElementById('welcome');
+                var name =document.getElementById('name');
+
+                welcome_tori.innerHTML = "Hello " + name.value + "!";
+            }
+    
+    </script>
+</head>  
+<body>
+    <p id = "welcome"> </p> 
+
+    <form>
+        what is your name: <input type="text" id="name" /><br/>
+        <input type="button" value="Done" onclick="write_name();" />
+    </form>
+</body>	
 
 </html>

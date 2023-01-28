a14c442cf628c4c6b02d49ad22ad57175e195acd
VictoriaL23 <83440406+VictoriaL23@users.noreply.github.com>
2023-01-28 15:21:33 -0500

    Add files via upload

---
+++ Quick form tuturial.js
@@ -0,0 +1,13 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Forms </title>
+        <script type="text/javascript">
+
+        </script>
+    </head>    
+    <body>
+    <p id = "Welcome to the form"> </p>    
+        </body>    
+
+</html>
